473e30ff5649f62545b7b19870b302b58b6c3f22
dixonhome47-sketch <dixonhome47@gmail.com>
2026-06-15 10:25:51 +1000

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // This file is the only thing you need to edit between deployments.
 
 window.FA_CONFIG = {
-  ANTHROPIC_API_KEY: 'sk-ant-api03-nOK...DgAA',
+  ANTHROPIC_API_KEY:'sk-ant-api03-nOK...DgAA',
   MODEL: 'claude-haiku-4-5',
   APP_VERSION: '4.1',
 };
