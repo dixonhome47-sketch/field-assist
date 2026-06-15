4f45a9b47c7fadecae6032a71f79244e9015cb09
dixonhome47-sketch <dixonhome47@gmail.com>
2026-06-15 12:12:59 +1000

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // This file is the only thing you need to edit between deployments.
 
 window.FA_CONFIG = {
-ANTHROPIC_API_KEY: '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',
+  ANTHROPIC_API_KEY: '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',
   MODEL: 'claude-haiku-4-5-20251001',
   APP_VERSION: '4.1',
 };
